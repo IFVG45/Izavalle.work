f534004250c18a83fd6b9c4c3849483acdf967a8
izavalle23 <chikis27@hotmail.ca>
2017-10-05 13:27:06 -0400

    chanigng things

--- css/js/main.js
+++ css/js/main.js
@@ -1,6 +1,3 @@
-var $navBtn = $('.toggle-button');
-var $nav = $('.toggle-nav');
-
-$navBtn.on('click', function () {
-	$nav.toggleClass('toggle-nav-open');
-})
+$('.scroll').localScroll({
+	target: 'body'
+});
